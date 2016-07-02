1cb806ed68b5cb4a767229b8ad4fceda150dd2e5
kunta0514 <kunta0514@163.com>
2016-07-02 11:40:48 +0800

    fix bug

--- database/seeds/ConsultantSeeder.php
+++ database/seeds/ConsultantSeeder.php
@@ -15,26 +15,91 @@ class ConsultantSeeder extends Seeder
     {
         //
         $consultantList=[
-            'name'=>'朱文静',
-            'occupational_level'=> '问题专家',
-            'occupational_age'=>5,
-            'tel'=>'138001380000',
-            'sale_comment'=>'江南新天地 华天明紫阳府',
-            'answer_count'=>205,
-            'deal_count'=>87,
-            'people_count'=>300,
+            [
+                'name'=>'朱文静',
+                'occupational_level'=> '问题专家',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>205,
+                'deal_count'=>87,
+                'people_count'=>300,
+                'good_rate'=>89
+            ],
+            [
+                'name'=>'张三丰',
+                'occupational_level'=> '问题专家',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>33,
+                'deal_count'=>112,
+                'people_count'=>345,
+                'good_rate'=>89
+            ],
+            [
+                'name'=>'陈冠希',
+                'occupational_level'=> '情感专家',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>123,
+                'deal_count'=>31,
+                'people_count'=>542,
+                'good_rate'=>90
+            ],
+            [
+                'name'=>'AngleBaby',
+                'occupational_level'=> '颜值担当',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>205,
+                'deal_count'=>87,
+                'people_count'=>300,
+                'good_rate'=>89
+            ],
+            [
+                'name'=>'罗永浩',
+                'occupational_level'=> '相声老师',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>205,
+                'deal_count'=>87,
+                'people_count'=>300,
+                'good_rate'=>89
+            ],
+            [
+                'name'=>'雷军',
+                'occupational_level'=> '英语专家',
+                'occupational_age'=>5,
+                'tel'=>'138001380000',
+                'sale_comment'=>'江南新天地 华天明紫阳府',
+                'answer_count'=>205,
+                'deal_count'=>87,
+                'people_count'=>300,
+                'good_rate'=>89
+            ]
         ];
-//        DB::table('consultant')->truncate();
+        
+        DB::table('consultant')->truncate();
 
-        Consultant::create([
-            'name' => $consultantList['name'],
-            'occupational_level' => $consultantList['occupational_level'],
-            'occupational_age' => $consultantList['occupational_age'],
-            'tel' => $consultantList['tel'],
-            'sale_comment' => $consultantList['sale_comment'],
-            'answer_count' => $consultantList['answer_count'],
-            'deal_count' => $consultantList['deal_count'],
-            'people_count' => $consultantList['people_count']
-        ]);
+        foreach($consultantList as $consultant)
+        {
+            Consultant::create([
+                'name' => $consultant['name'],
+                'occupational_level' => $consultant['occupational_level'],
+                'occupational_age' => $consultant['occupational_age'],
+                'tel' => $consultant['tel'],
+                'sale_comment' => $consultant['sale_comment'],
+                'answer_count' => $consultant['answer_count'],
+                'deal_count' => $consultant['deal_count'],
+                'people_count' => $consultant['people_count'],
+                'good_rate'=>$consultant['good_rate'],
+            ]);
+        }
+
+
     }
 }
